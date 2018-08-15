97162bb28ec25c3a390265b8db9c8184cb442449
ehu <fehuehuf@gmail.com>
2018-08-15 23:23:25 +0900

    Fragmentの作成

---
+++ app/src/main/java/com/example/ehu/weekckech/MainPagerWeekFragment.kt
@@ -0,0 +1,20 @@
+package com.example.ehu.weekckech
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class MainPagerWeekFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.pager_week, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/ehu/weekckech/MianPagerDayFragment.kt
@@ -0,0 +1,24 @@
+package com.example.ehu.weekckech
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class BlankFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.pager_day, container, false)
+    }
+
+
+}
