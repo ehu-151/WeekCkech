3d28cb43b92deebfecff91e3d11bd7cb7f156288
ehu <fehuehuf@gmail.com>
2018-08-24 16:51:13 +0900

    modelの生成

--- app/src/main/java/com/example/ehu/weekckech/data/sql/DayListItemModel.kt
+++ app/src/main/java/com/example/ehu/weekckech/data/sql/DayListItemModel.kt
@@ -1,5 +1,7 @@
 package com.example.ehu.weekckech.data.sql
 
+import java.util.*
+
 /**
  * 日ページのリストアイテムのDTO
  *
@@ -7,5 +9,31 @@ package com.example.ehu.weekckech.data.sql
  * @property [title] アイテムのタイトル
  * @property [detail] アイテムの詳細
  */
-data class DayListItemModel (val isChecked:Boolean, val title:String, val detail:String){
+data class DayListItemModel (val isChecked:Boolean, val title:String, val detail:String,
+                             var deadline:Date,val notice:CharArray,val weekGroup:String) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as DayListItemModel
+
+        if (isChecked != other.isChecked) return false
+        if (title != other.title) return false
+        if (detail != other.detail) return false
+        if (deadline != other.deadline) return false
+        if (!Arrays.equals(notice, other.notice)) return false
+        if (weekGroup != other.weekGroup) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = isChecked.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + detail.hashCode()
+        result = 31 * result + deadline.hashCode()
+        result = 31 * result + Arrays.hashCode(notice)
+        result = 31 * result + weekGroup.hashCode()
+        return result
+    }
 }
