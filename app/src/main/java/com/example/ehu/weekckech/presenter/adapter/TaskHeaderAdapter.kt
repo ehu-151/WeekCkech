854647a4e706eef7c0953d328309b17cec016a59
ehu <fehuehuf@gmail.com>
2018-08-28 15:33:35 +0900

    ListHeaderのadapter作成

--- app/src/main/java/com/example/ehu/weekckech/presenter/adapter/TaskHeaderAdapter.kt
+++ app/src/main/java/com/example/ehu/weekckech/presenter/adapter/TaskHeaderAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.ehu.weekckech.presenter.adapter
 
 import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.ehu.weekckech.R
 
@@ -12,9 +15,24 @@ class TaskHeaderAdapter(var context: Context, var headerName: String)
     : BaseAdapter() {
     val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        var v=convertView
+        var v = convertView
         var holder: TaskHeaderAdapter.CustomViewHolder? = null
-        v = inflater.inflate(R.layout.pager_day_listitem, null)
+        v = inflater.inflate(R.layout.pager_day_listheader, null)
+        // CustomViewHolderとlayoutの関連付け
+        holder = TaskHeaderAdapter.CustomViewHolder(
+                v.findViewById(R.id.header_title) as TextView,
+                v.findViewById(R.id.header_imageView) as ImageView
+        )
+        // リサイクルするときのためにタグ付けしておく
+        v.tag = holder
+
+        // headerNameのセット
+        holder.headerName.text = headerName
+
+        // クリックアダプターのセット
+        v.findViewById<ConstraintLayout>(R.id.header_imageView).setOnClickListener {
+            Log.d("setOnClickListener", "header_imageView:"+position.toString())
+        }
         return v as View
     }
 
@@ -29,8 +47,10 @@ class TaskHeaderAdapter(var context: Context, var headerName: String)
     override fun getCount(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
     // 表示するビューの型指定
     class CustomViewHolder(
-            var headerName:TextView
+            var headerName: TextView,
+            var addImageView: ImageView
     )
 }
