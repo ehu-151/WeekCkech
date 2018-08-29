791a749f820e6304ebb0e3cf302d38c7347aadf9
ehu <fehuehuf@gmail.com>
2018-08-29 10:02:56 +0900

    fragmentの仮挿入

--- app/src/main/java/com/example/ehu/weekckech/presenter/activity/AddEditTaskActivity.kt
+++ app/src/main/java/com/example/ehu/weekckech/presenter/activity/AddEditTaskActivity.kt
@@ -1,12 +1,9 @@
 package com.example.ehu.weekckech.presenter.activity
 
-import android.app.Fragment
-import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ehu.weekckech.R
-import com.example.ehu.weekckech.view.fragment.AddEditFragment
-import android.graphics.Color.parseColor
+import com.example.ehu.weekckech.view.fragment.AddEditTaskFragment
 import android.view.WindowManager
 import android.os.Build
 import android.support.v4.content.ContextCompat
@@ -16,7 +13,7 @@ class AddEditTaskActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_add_edit)
+        setContentView(R.layout.activity_add_edit_task)
 
         // StatusBarの色を変更
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {// Android 5.0 Lollipop
@@ -29,8 +26,9 @@ class AddEditTaskActivity : AppCompatActivity() {
             window.statusBarColor = ContextCompat.getColor(this,R.color.colorSub)
         }
         // Fragmentのセット
-        val fragment= AddEditFragment()
+        val fragment= AddEditTaskFragment()
         val manege = supportFragmentManager.beginTransaction()
-//        manege.add(fragment)
+        manege.add(R.id.add_fragment_space,fragment)
+        manege.commit()
     }
 }
--- app/src/main/java/com/example/ehu/weekckech/view/fragment/AddEditTaskFragment.kt
+++ app/src/main/java/com/example/ehu/weekckech/view/fragment/AddEditTaskFragment.kt
@@ -10,11 +10,11 @@ import android.view.ViewGroup
 import com.example.ehu.weekckech.R
 
 
-class AddEditFragment : Fragment() {
+class AddEditTaskFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_add_edit, container, false)
+        return inflater.inflate(R.layout.fragment_add_edit_task, container, false)
     }
 
 
