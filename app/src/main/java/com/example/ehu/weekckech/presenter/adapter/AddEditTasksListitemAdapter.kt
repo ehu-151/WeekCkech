eb5f89c2dfc578212cc4067dd1363eada7a232a3
ehu <fehuehuf@gmail.com>
2018-10-18 21:19:42 +0900

    開発再開

--- app/src/main/java/com/example/ehu/weekckech/presenter/adapter/AddEditTasksListitemAdapter.kt
+++ app/src/main/java/com/example/ehu/weekckech/presenter/adapter/AddEditTasksListitemAdapter.kt
@@ -6,11 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import com.example.ehu.weekckech.R
-import com.example.ehu.weekckech.R.id.spinner
 import com.example.ehu.weekckech.data.sql.AddEditTaskItemModel
 import com.example.ehu.weekckech.presenter.contract.AddEditTaskContract
-import android.widget.ArrayAdapter
-
 
 
 class AddEditTasksListitemAdapter(
@@ -69,8 +66,10 @@ class AddEditTasksListitemAdapter(
     }
 
     fun setupSpinnerItem(spinner: Spinner, menu: ArrayList<String>?) {
-        val adapter = ArrayAdapter<String>(mContext, android.R.layout.simple_spinner_item)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // スピナーのレイアウト指定
+        val adapter = ArrayAdapter<String>(mContext, R.layout.spinner_item)
+        // プルダウンレイアウト指定
+        adapter.setDropDownViewResource(R.layout.spinner_item)
         adapter.addAll(menu)
         spinner.adapter=adapter
     }
