b6e0bdfb1d86f3b26d6ede623eeb8592ff904734
ehu <fehuehuf@gmail.com>
2018-10-18 21:54:01 +0900

    保存する、やめるボタンの追加

--- app/src/main/java/com/example/ehu/weekckech/view/fragment/AddEditTaskFragment.kt
+++ app/src/main/java/com/example/ehu/weekckech/view/fragment/AddEditTaskFragment.kt
@@ -4,9 +4,11 @@ package com.example.ehu.weekckech.view.fragment
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ListView
 import com.example.ehu.weekckech.R
 import com.example.ehu.weekckech.data.sql.AddEditTaskItemModel
@@ -15,9 +17,12 @@ import com.example.ehu.weekckech.presenter.contract.AddEditTaskContract
 import com.example.ehu.weekckech.presenter.presenter.AddEditTaskPresenter
 
 
-class AddEditTaskFragment : Fragment() ,AddEditTaskContract.View{
-    lateinit var mContext:Context
-    lateinit var listView:ListView
+class AddEditTaskFragment : Fragment(), AddEditTaskContract.View {
+    val TAG = "AddEditTaskFragment"
+    lateinit var mContext: Context
+    lateinit var listView: ListView
+    lateinit var saveButton: Button
+    lateinit var leaveButton: Button
     override var presenter: AddEditTaskContract.Presenter = AddEditTaskPresenter(this)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -31,12 +36,18 @@ class AddEditTaskFragment : Fragment() ,AddEditTaskContract.View{
         mContext = view.context
         // ListViewのセット
         listView = view.findViewById(R.id.listView)
+        // Buttonのセット
+        saveButton = view.findViewById(R.id.edit_save_button)
+        leaveButton = view.findViewById(R.id.edit_leave_button)
+        // clickadapter
+        saveButton.setOnClickListener { Log.v(TAG, "clicked") }
+        leaveButton.setOnClickListener { Log.v(TAG, "clicked") }
 
         presenter.start()
     }
 
-    override fun showTaskConfigEditRow(listItemModel:ArrayList<AddEditTaskItemModel>) {
-        listView.adapter= AddEditTasksListitemAdapter(mContext,listItemModel,presenter)
+    override fun showTaskConfigEditRow(listItemModel: ArrayList<AddEditTaskItemModel>) {
+        listView.adapter = AddEditTasksListitemAdapter(mContext, listItemModel, presenter)
     }
 
     override fun showTasksMain() {
