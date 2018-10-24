f967e1486faee0fb11f989f9b5492ce86b300a0d
ehu <fehuehuf@gmail.com>
2018-10-24 15:55:06 +0900

    初期で12:00と表示

--- app/src/main/java/com/example/ehu/weekckech/presenter/presenter/AddEditTaskPresenter.kt
+++ app/src/main/java/com/example/ehu/weekckech/presenter/presenter/AddEditTaskPresenter.kt
@@ -18,9 +18,9 @@ class AddEditTaskPresenter(val addEditTaskView: AddEditTaskContract.View) : AddE
     override fun loadTaskConfigEditRow() {
         var lists = java.util.ArrayList<AddEditTaskItemModel>()
         val model = AddEditTaskItemModel
-        lists.add(AddEditTaskItemModel(R.id.edit_include_title, R.drawable.ic_title_white_24dp, model.EDITTEXT, "タイトル"))
-        lists.add(AddEditTaskItemModel(R.id.edit_include_detail, R.drawable.ic_notes_white_24dp, model.EDITTEXT, "詳細"))
-        lists.add(AddEditTaskItemModel(R.id.edit_include_limittime, R.drawable.ic_access_time_white_24dp, model.TEXTVIEW))
+        lists.add(AddEditTaskItemModel(R.id.edit_include_title, R.drawable.ic_title_white_24dp, model.EDITTEXT, hintText = "タイトル"))
+        lists.add(AddEditTaskItemModel(R.id.edit_include_detail, R.drawable.ic_notes_white_24dp, model.EDITTEXT, hintText = "詳細"))
+        lists.add(AddEditTaskItemModel(R.id.edit_include_limittime, R.drawable.ic_access_time_white_24dp, model.TEXTVIEW,text = "12:00"))
         lists.add(AddEditTaskItemModel(R.id.edit_include_notificationtime, R.drawable.ic_notifications_white_24dp, model.SPINNER, spinnerItem = arrayListOf("1H前", "2H前", "12時")))
         lists.add(AddEditTaskItemModel(R.id.edit_include_weekgroup, R.drawable.ic_today_white_24dp, model.SPINNER, spinnerItem = arrayListOf("月", "火", "水", "木", "金", "土", "日")))
         addEditTaskView.showTaskConfigEditRow(lists)
--- app/src/main/java/com/example/ehu/weekckech/view/fragment/AddEditTaskFragment.kt
+++ app/src/main/java/com/example/ehu/weekckech/view/fragment/AddEditTaskFragment.kt
@@ -5,8 +5,6 @@ import android.content.Context
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.v4.app.Fragment
-import android.text.Layout
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -84,7 +82,7 @@ class AddEditTaskFragment : Fragment(), AddEditTaskContract.View {
                 adapter.addAll(list.spinnerItem)
                 layout.findViewById<Spinner>(R.id.spinner).adapter = adapter
             } else if (list.componentType == model.TEXTVIEW) {
-//                layout.findViewById<TextView>(R.id.textView).text = ""
+                layout.findViewById<TextView>(R.id.textView).text = list.text
             }
         }
 
