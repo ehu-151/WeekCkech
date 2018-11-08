aa672d3ac4dc451d357fa42e1c2b058bdd3dc990
ehu <fehuehuf@gmail.com>
2018-11-08 20:01:02 +0900

    DataBinding使用

--- app/src/main/java/com/example/ehu/weekckech/view/fragment/MainPagerDayFragment.kt
+++ app/src/main/java/com/example/ehu/weekckech/view/fragment/MainPagerDayFragment.kt
@@ -1,26 +1,21 @@
 package com.example.ehu.weekckech.view.fragment
 
-
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
 import com.example.ehu.weekckech.R
 import com.example.ehu.weekckech.data.sql.DayListItemModel
+import com.example.ehu.weekckech.databinding.PagerDayBinding
 import com.example.ehu.weekckech.presenter.activity.AddEditTaskActivity
-import com.example.ehu.weekckech.presenter.activity.TasksMainActivity
 import com.example.ehu.weekckech.presenter.adapter.TasksAdapter
 import com.example.ehu.weekckech.presenter.contract.PagerDayConstract
 import com.example.ehu.weekckech.presenter.presenter.PagerDayPresenter
-import kotlinx.android.synthetic.main.pager_day.view.*
-import se.emilsjolander.stickylistheaders.StickyListHeadersListView
-import kotlin.collections.ArrayList
+
 
 /**
  * PagerDayConstract.Viewの実装メソッドはPagerDayPresenterから呼ばれることを想定しています。
@@ -31,13 +26,11 @@ import kotlin.collections.ArrayList
 class MainPagerDayFragment : Fragment(), PagerDayConstract.View {
     // セットする変数の宣言
     override var presenter: PagerDayConstract.Presenter = PagerDayPresenter(this)
-    lateinit var listView: StickyListHeadersListView
-    lateinit var floatingActionButton: FloatingActionButton
-
-
+    lateinit var binding: PagerDayBinding
     private lateinit var mContext: Context
+
     override fun showDaysTasks(dayListItems: ArrayList<DayListItemModel>) {
-        listView.adapter = TasksAdapter(mContext, dayListItems, presenter)
+        binding.listView.adapter = TasksAdapter(mContext, dayListItems, presenter)
     }
 
     override fun showAddEditTask() {
@@ -51,7 +44,10 @@ class MainPagerDayFragment : Fragment(), PagerDayConstract.View {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.pager_day, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.pager_day, container, false)
+        val root = binding.getRoot()
+        binding.presenter = presenter
+        return root
     }
 
     override fun onResume() {
@@ -64,14 +60,5 @@ class MainPagerDayFragment : Fragment(), PagerDayConstract.View {
         super.onViewCreated(view, savedInstanceState)
         // Contextの格納
         mContext = view.context
-        // ListViewのセット
-        listView = view.findViewById(R.id.listView)
-        floatingActionButton = view.findViewById(R.id.add_task_floating_button)
-        floatingActionButton.setOnClickListener {
-            Log.d("setOnClickListener", "Add Task")
-            presenter.addNewDayTask()
-        }
-
     }
-
 }
