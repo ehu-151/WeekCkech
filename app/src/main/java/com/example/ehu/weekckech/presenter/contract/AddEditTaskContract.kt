d9dd1fe7d94cccc257fefb5cc38dff00208c316f
ehu <fehuehuf@gmail.com>
2019-06-26 03:28:55 +0900

    不要なクラスの削除

--- app/src/main/java/com/example/ehu/weekckech/presenter/contract/AddEditTaskContract.kt
+++ app/src/main/java/com/example/ehu/weekckech/presenter/contract/AddEditTaskContract.kt
@@ -3,11 +3,10 @@ package com.example.ehu.weekckech.presenter.contract
 import android.content.Context
 import com.example.ehu.weekckech.BasePresenter
 import com.example.ehu.weekckech.BaseView
-import com.example.ehu.weekckech.data.sql.AddEditTaskItemModel
 import com.example.ehu.weekckech.data.sql.TaskDataModel
 
 interface AddEditTaskContract {
-    interface View : BaseView<AddEditTaskContract.Presenter> {
+    interface View : BaseView<Presenter> {
         fun setTaskConfigEditRow(model: TaskDataModel)
         fun showTasksMain()
         fun showKeyboard()
